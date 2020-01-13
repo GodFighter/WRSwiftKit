0b128667d394fa5e70da00928d3db1f86af31b0d
God_Fighter <xianghui_ios@163.com>
2020-01-14 00:04:05 +0800

    swift demo

--- Example/Swift/WRSwiftKit/ViewController.swift
+++ Example/Swift/WRSwiftKit/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import WRSwiftKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("\(WRDevice.isIpad) \n \(WRDevice.safeAreaInset) \n \(Info.appName)")
+
     }
 
     override func didReceiveMemoryWarning() {
